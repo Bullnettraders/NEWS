42be5aecfb8639cc5e5c4cd31a8cee16fa53bf1d
Bullnettraders <witz.m@hotmail.com>
2025-05-14 20:17:14 +0200

    poster.py

--- poster.py
+++ poster.py
@@ -1,14 +1,11 @@
-# poster.py
-
 import discord
-from config import CHANNEL_ID
 
-async def post_news(news_item, client):
-    channel = client.get_channel(CHANNEL_ID)
+async def post_news(news_item, client, channel_id):
+    channel = client.get_channel(channel_id)
     if channel:
         embed = discord.Embed(
-            title="🚨 [BREAKING] " + news_item["translated_title"],
-            description=f"Quelle: {news_item['source']}\n[Link zur News]({news_item['link']})",
-            color=discord.Color.red()
+            title=f"🚨 {news_item['translated_title']}",
+            description=f"[Zur Quelle]({news_item['link']})",
+            color=discord.Color.blue()
         )
         await channel.send(embed=embed)
