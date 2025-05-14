a5f0101962c916f967987b48fabd2435bf8a76f1
Bullnettraders <witz.m@hotmail.com>
2025-05-14 21:00:45 +0200

    poster.py

--- poster.py
+++ poster.py
@@ -4,17 +4,15 @@ async def post_news(news_item, client, channel_id):
     channel = client.get_channel(channel_id)
     if channel:
         embed = discord.Embed(
-            title=f"{news_item['translated_title']}",
-            description=f"[Zur Quelle]({news_item['link']})",
+            title=news_item["title"],
+            description=f"[👉 Hier klicken zur Quelle]({news_item['link']})",
             color=discord.Color.blue()
         )
-        
-        # Bild hinzufügen, wenn verfügbar
-        if 'image' in news_item:
-            embed.set_thumbnail(url=news_item['image'])
 
-        # Summary hinzufügen, wenn verfügbar
-        if 'summary' in news_item:
-            embed.add_field(name="Zusammenfassung", value=news_item['summary'], inline=False)
-        
+        if "summary" in news_item and news_item["summary"]:
+            embed.add_field(name="Zusammenfassung", value=news_item["summary"][:200] + "...", inline=False)
+
+        if "image" in news_item and news_item["image"]:
+            embed.set_thumbnail(url=news_item["image"])
+
         await channel.send(embed=embed)
