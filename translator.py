1c6f219bc1281a1de6dcf02dc925200dfd91cea6
Bullnettraders <witz.m@hotmail.com>
2025-05-14 20:16:03 +0200

    translator.py

--- translator.py
+++ translator.py
@@ -1,5 +1,3 @@
-# translator.py
-
 from deep_translator import GoogleTranslator
 
 def translate_text(text, target_language='de'):
@@ -7,4 +5,4 @@ def translate_text(text, target_language='de'):
         return GoogleTranslator(source='auto', target=target_language).translate(text)
     except Exception as e:
         print(f"Übersetzungsfehler: {e}")
-        return text  # Falls Übersetzung fehlschlägt, Originaltext zurückgeben
+        return text
