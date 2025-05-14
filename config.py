239f627604d8416382b0376350197434cd00a819
Bullnettraders <witz.m@hotmail.com>
2025-05-14 21:15:53 +0200

    config.py

--- config.py
+++ config.py
@@ -5,17 +5,18 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 FETCH_INTERVAL = 60  # Sekunden
 
 RSS_FEEDS_CHANNELS = {
-    # Trump Channel
-    "https://rss.app/feeds/6ylmybWwgG1OqovY.xml": 1371879824997744791,  # Trump News allgemein
-    "https://rss.app/feeds/4NfbW8s5xqV7G7M4.xml": 1371879824997744791,  # Trump's eigene Truth Posts
+    # Trump News
+    "https://rss.politico.com/donald-trump.xml": 1371879824997744791,
 
-    # Andere Channels
+    # n-tv News
     "https://www.n-tv.de/rss": 1371879772367622225,
+
+    # Weitere Feeds
     "https://rss.app/feeds/LHqcalDfUI6gqB33.xml": 1371879882258124840,  # Investing
-    "https://rss.app/feeds/BCVWNnbILyMc16Px.xml": 1371889076009177119,  # Watcher Guru (mit Bild)
+    "https://rss.app/feeds/BCVWNnbILyMc16Px.xml": 1371889076009177119,  # Watcher Guru
     "https://rss.app/feeds/tTF0IKUt7fim3O2Q.xml": 1372274450652860476,  # Times
 
-    # Sammelkanal für Wirtschaft
+    # Wirtschaftskanal
     "https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best": 1372212944926015560,
     "https://feeds.marketwatch.com/marketwatch/bulletins/": 1372212944926015560,
     "https://www.cnbc.com/id/100003114/device/rss/rss.html": 1372212944926015560,
