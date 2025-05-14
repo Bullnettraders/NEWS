081b680a7700a3822e4bc61b6944d9759ee82e74
Bullnettraders <witz.m@hotmail.com>
2025-05-14 16:09:28 +0200

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+DISCORD_TOKEN = "HIER_DEIN_BOT_TOKEN_EINFÜGEN"
+CHANNEL_ID = 1372212944926015560
+
+RSS_FEEDS = [
+    "https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best",
+    "https://www.cnbc.com/id/100003114/device/rss/rss.html",
+    "https://feeds.marketwatch.com/marketwatch/bulletins/"
+]
+
+FETCH_INTERVAL = 60  # alle 60 Sekunden abrufen
