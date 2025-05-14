56d38f8a0be54cd2fb5c21e13a4622672b048aac
Bullnettraders <witz.m@hotmail.com>
2025-05-14 20:59:37 +0200

    config.py

--- config.py
+++ config.py
@@ -1,24 +1,25 @@
 import os
 
-# Token aus Railway Environment Variable holen
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-# News-Abruf alle 60 Sekunden
 FETCH_INTERVAL = 60  # Sekunden
 
-# Alle Feeds + Channel-Zuordnung
 RSS_FEEDS_CHANNELS = {
-    "https://rss.app/feeds/6ylmybWwgG1OqovY.xml": 1371879824997744791,  # Trump News
-    "https://rss.app/feeds/4NfbW8s5xqV7G7M4.xml": 1371879824997744791,  # Trump eigene Posts
+    # Trump Channel
+    "https://rss.app/feeds/6ylmybWwgG1OqovY.xml": 1371879824997744791,  # Trump News allgemein
+    "https://rss.app/feeds/4NfbW8s5xqV7G7M4.xml": 1371879824997744791,  # Trump's eigene Truth Posts
+
+    # Andere Channels
     "https://www.n-tv.de/rss": 1371879772367622225,
-    "https://rss.app/feeds/LHqcalDfUI6gqB33.xml": 1371879882258124840,
-    "https://rss.app/feeds/BCVWNnbILyMc16Px.xml": 1371889076009177119,
-    "https://rss.app/feeds/tTF0IKUt7fim3O2Q.xml": 1372274450652860476,
+    "https://rss.app/feeds/LHqcalDfUI6gqB33.xml": 1371879882258124840,  # Investing
+    "https://rss.app/feeds/BCVWNnbILyMc16Px.xml": 1371889076009177119,  # Watcher Guru (mit Bild)
+    "https://rss.app/feeds/tTF0IKUt7fim3O2Q.xml": 1372274450652860476,  # Times
+
+    # Sammelkanal für Wirtschaft
     "https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best": 1372212944926015560,
     "https://feeds.marketwatch.com/marketwatch/bulletins/": 1372212944926015560,
     "https://www.cnbc.com/id/100003114/device/rss/rss.html": 1372212944926015560,
     "https://feeds.finance.yahoo.com/rss/2.0/headline?s=^DJI&region=US&lang=en-US": 1372212944926015560,
     "https://feeds.oilprice.com/oilpricecom/BreakingNews": 1372212944926015560,
     "https://www.coindesk.com/arc/outboundfeeds/rss/": 1372212944926015560,
-    "https://rss.app/feeds/4NfbW8s5xqV7G7M4.xml": 1371879824997744791,  # Trump's eigene Truth Social Posts
 }
